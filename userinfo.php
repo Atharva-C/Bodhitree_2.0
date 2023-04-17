6559e9298d6faedec31fdb2949e1577405cf7e31
Atharva Chaudhari <92793651+Atharva-C@users.noreply.github.com>
2023-04-18 00:19:56 +0530

    MAde a dynamic website for e learning platform bodhitree 2.0

---
+++ userinfo.php
@@ -0,0 +1,29 @@
+<?php
+
+$con = mysqli_connect('127.0.0.1:4306','root','','bodhitree');
+
+if($con){
+    echo "Connection Successful";
+}else{
+    echo "No Connection!";
+}
+
+mysqli_select_db($con, 'bodhitree');
+
+$Username = $_POST['Username'];
+$EmailID = $_POST['EmailID'];
+$PhoneNo = $_POST['PhoneNo'];
+// $Comments = $_POST['Comments'];
+
+$sql= " INSERT INTO `userinfodata` (`Username`, `EmailID`, `PhoneNo`) values ( '$Username', '$EmailID', '$PhoneNo' )";
+
+// insert in database 
+ $rs=mysqli_query($con, $sql);
+//  die(mysqli_error());
+
+if($rs)
+{
+	echo "Contact Records Inserted <br> <br>";
+
+}
+?>
